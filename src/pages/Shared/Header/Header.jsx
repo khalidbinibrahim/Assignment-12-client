105739d7aebdee97c3d11087140a9d4033164a41
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-06-01 15:05:34 +0600

    header completed

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                             <Fade direction="up" triggerOnce={true}><button to="/add_tourists_spot" className="btn bg-[#F7A582] border-none text-white font-semibold text-base font-sourceSans3 rounded-md px-7">View More <i className='text-xl'><MdPets /></i></button></Fade>
                         </div>
 
-                        <div className="w-[270px]">
+                        <div className="w-[270px] font-sourceSans3">
                             <Swiper
                                 speed={600}
                                 autoplay={{
@@ -32,44 +32,44 @@ const Header = () => {
                                 className="mySwiper"
                             >
                                 <SwiperSlide>
+                                    <div className="bg-[url('https://images.unsplash.com/photo-1567270671170-fdc10a5bf831?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGV0JTIwcnVzc2lhbnxlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
+                                        <div className="title" data-swiper-parallax="-300">
+                                            <p className="absolute top-80 font-medium text-4xl text-white">Russian</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
                                     <div className="bg-[url('https://plus.unsplash.com/premium_photo-1674236550362-58a7fdd7f899?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBldCUyMGNhbmFkaWFufGVufDB8fDB8fHww')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
                                         <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-80 font-normal text-4xl text-white font-bebasNeue">Canadian</p>
+                                            <p className="absolute top-80 font-medium text-4xl text-white">Canadian</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
-                                    <div className="bg-[url('https://images.unsplash.com/photo-1565803974275-dccd2f933cbb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dm9sdW50ZWVyaW5nfGVufDB8fDB8fHww')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
+                                    <div className="bg-[url('https://images.unsplash.com/photo-1422565096762-bdb997a56a84?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBldCUyMHVzYXxlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
                                         <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-80 font-normal text-4xl text-white font-bebasNeue">Barbados</p>
+                                            <p className="absolute top-80 font-medium text-4xl text-white">American</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
-                                    <div className="bg-[url('https://images.unsplash.com/photo-1670405564800-bbb09765317b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHZvbHVudGVlcmluZ3xlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
+                                    <div className="bg-[url('https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGV0JTIwdW5pdGVkJTIwa2luZ2RvbXxlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
                                         <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-80 font-normal text-4xl text-white font-bebasNeue">Italy</p>
+                                            <p className="absolute top-80 font-medium text-4xl text-white">British</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
-                                    <div className="bg-[url('https://plus.unsplash.com/premium_photo-1681152790484-8c0beab3999a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dm9sdW50ZWVyaW5nfGVufDB8fDB8fHww')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
+                                    <div className="bg-[url('https://images.unsplash.com/photo-1570914296680-ad86f42783c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGV0JTIwdHVya2V5fGVufDB8fDB8fHww')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
                                         <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-72 font-normal text-4xl text-white font-bebasNeue">República dominicana</p>
+                                            <p className="absolute top-80 font-medium text-4xl text-white">Turkish</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
-                                    <div className="bg-[url('https://images.unsplash.com/photo-1655573988224-2484b832dd5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHZvbHVudGVlcmluZ3xlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
+                                    <div className="bg-[url('https://images.unsplash.com/photo-1520981988395-08b47cfa6865?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGV0JTIwZ2VybWFueXxlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
                                         <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-80 font-normal text-4xl text-white font-bebasNeue">Turkey</p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="bg-[url('https://images.unsplash.com/photo-1559027615-cd4628902d4a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZvbHVudGVlcmluZ3xlbnwwfHwwfHx8MA%3D%3D')] bg-center bg-no-repeat bg-cover rounded-lg h-[415px] overflow-hidden w-full p-5">
-                                        <div className="title" data-swiper-parallax="-300">
-                                            <p className="absolute top-80 font-normal text-4xl text-white font-bebasNeue">Nepal</p>
+                                            <p className="absolute top-80 font-medium text-4xl text-white">German</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
